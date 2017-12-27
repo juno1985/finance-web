b067109b19a9942ae969a19c3545a9177417a570
Jun <wangzhen_tju@126.com>
2017-12-27 17:36:02 +0800

    添加导航页面

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
