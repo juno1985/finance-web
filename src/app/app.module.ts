80690ad0f2a8c61d336d164aef7f97bd22c3f1a8
Jun <wangzhen_tju@126.com>
2017-12-27 21:28:36 +0800

    增加路由和css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-
+import { Routes,RouterModule } from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+const routeConfig: Routes =[
+   {path:'', component:HomeComponent}
+ ]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routeConfig),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
