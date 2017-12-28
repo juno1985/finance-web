ce479def89527352907c7d4ce00997fc0684cc7e
Jun <wangzhen_tju@126.com>
2017-12-28 17:45:08 +0800

    添加表单验证,转账金额必须大于0

--- src/app/pro-input/pro-input.component.ts
+++ src/app/pro-input/pro-input.component.ts
@@ -1,4 +1,7 @@
+import { ItemService } from './../shared/item.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder,FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-pro-input',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProInputComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  formModel:FormGroup;
+
+  constructor() { 
+    let fb = new FormBuilder();
+    this.formModel=fb.group({
+      //默认空，验证是否是正数
+      tranAccount:[null,this.positiveNumberValidator]
+    });
   }
 
+  ngOnInit() {
+    /** 这里需要使用item.service得到服务端数据填充下拉菜单*/
+  }
+
+  positiveNumberValidator(control:FormControl):any{
+
+    let amcct=parseInt(control.value);
+
+
+    if(!amcct&&amcct!=0)
+    return null;
+
+    
+    if(amcct>0){
+        return null;}
+    else
+      { return {positiveNum:true};}
+  }
+
+  onSearch(){
+       
+     //表单全部验证过才能提交
+     if(this.formModel.valid){
+      console.log(this.formModel.value);
+     }
+  }
 }
